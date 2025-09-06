e971fb93fc927e9e5f695321977eafe222d22568
noracami <kerke.2011@gmail.com>
2025-09-07 01:37:18 +0800

    fix gh-page error by https://israynotarray.com/other/20220928/1377668819/

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/hexSchool-Vue/',   // 要佈署上 GitHub 才能改回 /por7/ 
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/forked-hexSchool-Vue/', // 要佈署上 GitHub 才能改回 /por7/
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          // substr 是被淘汰語法，因此要改 slice
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+        },
+      },
+    },
     outDir: 'dist', // 輸出到根目錄的 dist
     emptyOutDir: true,
   },
